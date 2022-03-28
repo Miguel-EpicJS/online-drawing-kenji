f19addac3f8d2d36b6c775f04f4078a6a68c7750
BgFerreira <brunogabriel.ff@gmail.com>
2022-03-28 19:07:31 -0300

    Commit do https base

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
 //------------- HTTPS ----------------
+require("dotenv").config();
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT;
+
+const server = https.createServer({
+    cert: fs.readFileSync(path.join(__dirname, "certs", "mycert.crt")),
+    key: fs.readFileSync(path.join(__dirname, "certs", "selfsigned.key"))
+}, app);
+
+// ------------ Apenas para testar --------------
+//app.listen(PORT, () => {
+//    console.log("Https connection is working");
+//});
