edea3680ee49d6872ffc624f4b7d3304e31e4014
Miguel Martinez <51725685+Miguel-EpicJS@users.noreply.github.com>
2022-03-29 12:53:16 -0300

    Basic backend/miguel (#6)
    * npm init
    
    * Installed the ws package
    
    * Added gitignore for node_modules
    
    * Simple working ws server(without https)
    
    * fixing git ignore for node_modules

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
 //------------- HTTPS ----------------
+
+// Simple http server for test ws, need to be changed
+
+const http = require("http");
+const WebSocket = require("ws");
+const wsServer = require("./src");
+
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end(index);
+});
+server.on("error", (err) => console.error(err));
+
+const wss = new WebSocket.Server({server, path: "/"});
+
+wsServer(wss, WebSocket);
+
+module.exports = { server, wss }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,51 @@
 // -------------- WEBSOCKET ----------------
+const routes = require("./routes/routes");
+
+const paths = Object.keys(routes);
+
+const crypto = require("crypto");
+
+const wsServer = (wss, WebSocket) => {
+
+    wss.on("connection", ws => {
+
+        ws.id = crypto.randomBytes(20).toString("hex");
+
+        ws.on("message", (data) => {
+
+            let receivedData;
+
+            try {
+                receivedData = JSON.parse(data.toString());
+            } catch (error) {
+
+                ws.send({ message: "Please submit a json" });
+                return 0;
+            }
+
+            if (receivedData.path) {
+
+                if (paths.indexOf(receivedData.path) >= 0) {
+
+                    routes[receivedData.path](receivedData, ws, wss, WebSocket);
+
+                } else {
+                    ws.send(JSON.stringify({ message: "This path don't exist", statusCode: 404 }));
+                }
+
+            } else {
+
+                ws.send(JSON.stringify({ message: "Please send the path of your data" }));
+
+            }
+
+        });
+
+        ws.onerror = function () {
+            console.log("Some Error occurred")
+        }
+    });
+
+};
+
+module.exports = wsServer;
