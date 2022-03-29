39e8b1ebd3ad1cd6eb82be5a3fed4a48f4ddf243
Layssa <layssa.lima166@gmail.com>
2022-03-29 13:48:33 -0300

    fix: structure repo

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,46 +6,44 @@ const paths = Object.keys(routes);
 const crypto = require("crypto");
 
 const wsServer = (wss, WebSocket) => {
+  wss.on("connection", (ws) => {
+    ws.id = crypto.randomBytes(20).toString("hex");
 
-    wss.on("connection", ws => {
+    ws.on("message", (data) => {
+      let receivedData;
+      
+      try {
+        receivedData = JSON.parse(data.toString());
+        console.log("--------------------- path ----------------");
+        console.log(receivedData.path);
+        console.log(receivedData);
+      } catch (error) {
+        ws.send({ message: "Please submit a json" });
+        return 0;
+      }
 
-        ws.id = crypto.randomBytes(20).toString("hex");
-
-        ws.on("message", (data) => {
-
-            let receivedData;
-
-            try {
-                receivedData = JSON.parse(data.toString());
-            } catch (error) {
-
-                ws.send({ message: "Please submit a json" });
-                return 0;
-            }
-
-            if (receivedData.path) {
-
-                if (paths.indexOf(receivedData.path) >= 0) {
-
-                    routes[receivedData.path](receivedData, ws, wss, WebSocket);
-
-                } else {
-                    ws.send(JSON.stringify({ message: "This path don't exist", statusCode: 404 }));
-                }
-
-            } else {
-
-                ws.send(JSON.stringify({ message: "Please send the path of your data" }));
-
-            }
-
-        });
-
-        ws.onerror = function () {
-            console.log("Some Error occurred")
+      if (receivedData.path) {
+        if (paths.indexOf(receivedData.path) >= 0) {
+          routes[receivedData.path](receivedData, ws, wss, WebSocket);
+        } else {
+          ws.send(
+            JSON.stringify({
+              message: "This path don't exist",
+              statusCode: 404,
+            })
+          );
         }
+      } else {
+        ws.send(
+          JSON.stringify({ message: "Please send the path of your data" })
+        );
+      }
     });
 
+    ws.onerror = function () {
+      console.log("Some Error occurred");
+    };
+  });
 };
 
 module.exports = wsServer;
