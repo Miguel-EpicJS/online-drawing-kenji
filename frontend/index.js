65e62d261f8158fd9b833d815af3c68dcfa9af3e
Miguel Martinez <51725685+Miguel-EpicJS@users.noreply.github.com>
2022-03-30 19:11:33 -0300

    Ws frontend/miguel (#9)
    * Comunication between frontend and backend for draws
    
    * Add modification to route draw for better use with frontend
    
    * Change from http to https

--- frontend/index.js
+++ frontend/index.js
@@ -10,8 +10,30 @@ canvas.height = window.innerHeight - canvasOffsetY;
 
 let isPainting = false;
 let lineWidth = 5;
+let color = "#000000";
 let startX;
 let startY;
+
+/* WS */
+
+ws = new WebSocket("wss://127.0.0.1:5050")
+
+ws.onmessage = (ms) => {
+
+    const submitedData = JSON.parse(ms.data);
+
+    ctx.strokeStyle = submitedData.drawing.color;
+    ctx.lineWidth = submitedData.drawing.lineWidth;
+    ctx.lineCap = 'round';
+
+    isPainting = false;
+
+    ctx.lineTo(submitedData.drawing.x, submitedData.drawing.y);
+    ctx.stroke();
+
+}; 
+
+/* DRAW */
 
 toolbar.addEventListener('click', e => {
     if (e.target.id === 'clear') {
@@ -21,6 +43,7 @@ toolbar.addEventListener('click', e => {
 
 toolbar.addEventListener('change', e => {
     if(e.target.id === 'stroke') {
+        color = e.target.value;
         ctx.strokeStyle = e.target.value;
     }
 
@@ -34,24 +57,29 @@ const draw = (e) => {
     if(!isPainting) {
         return;
     }
+    ws.send(JSON.stringify({path: "draw", lineWidth: lineWidth, x: e.clientX - canvasOffsetX, y: e.clientY, color: color}));
 
     ctx.lineWidth = lineWidth;
     ctx.lineCap = 'round';
 
     ctx.lineTo(e.clientX - canvasOffsetX, e.clientY);
     ctx.stroke();
+    console.log("mousemove");
 }
 
 canvas.addEventListener('mousedown', (e) => {
     isPainting = true;
     startX = e.clientX;
     startY = e.clientY;
+    console.log("mousedown");
 });
 
 canvas.addEventListener('mouseup', e => {
     isPainting = false;
     ctx.stroke();
     ctx.beginPath();
+    console.log("mouseup");
 });
 
 canvas.addEventListener('mousemove', draw);
+
