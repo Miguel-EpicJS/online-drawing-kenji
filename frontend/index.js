7e34980deec60c8817a7d2e947beb050a3900c3e
Layssa <layssa.lima166@gmail.com>
2022-03-31 01:32:46 -0300

    refactor/debug: try resolve bug

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById("drawing-board");
 const toolbar = document.getElementById("toolbar");
-const ctx = canvas.getContext("2d");
+const context = canvas.getContext("2d");
 const text = document.querySelector(".show-answer");
 const sendBtn = document.querySelector(".answer-button");
 const messageBox = document.querySelector(".answer-input");
@@ -9,83 +9,104 @@ const canvasOffsetX = canvas.offsetLeft;
 const canvasOffsetY = canvas.offsetTop;
 
 canvas.width = window.innerWidth - canvasOffsetX;
-canvas.height = window.innerHeight - canvasOffsetY;
+canvas.height = window.innerHeight - canvasOffsetY - 60;
 
-let isPainting = false;
-let lineWidth = 5;
-let color = "#000000";
-let startX;
-let startY;
+const paintStyle = {
+  lineWidth: 3,
+  color: "#000000",
+};
+
+const paint = {
+  active: false,
+  move: false,
+  pos: {
+    x: 0,
+    y: 0,
+  },
+  before: null,
+};
 
 /* WS */
-
 ws = new WebSocket("wss://localhost:5050");
 
 ws.onmessage = (ms) => {
   const submitedData = JSON.parse(ms.data);
 
-  ctx.strokeStyle = submitedData.drawing.color;
-  ctx.lineWidth = submitedData.drawing.lineWidth;
-  ctx.lineCap = "round";
+  context.strokeStyle = submitedData.drawing.color;
+  context.lineWidth = submitedData.drawing.lineWidth;
+  context.lineCap = "round";
 
-  isPainting = false;
+  paint.active = false;
   show(submitedData.drawing.text);
-  ctx.lineTo(submitedData.drawing.x, submitedData.drawing.y);
-  ctx.stroke();
+  context.lineTo(submitedData.drawing.x, submitedData.drawing.y);
+  context.stroke();
 };
 
 /* DRAW */
 
 toolbar.addEventListener("click", (e) => {
   if (e.target.id === "clear") {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    context.clearRect(0, 0, canvas.width, canvas.height);
   }
 });
 
 toolbar.addEventListener("change", (e) => {
   if (e.target.id === "stroke") {
-    color = e.target.value;
-    ctx.strokeStyle = e.target.value;
+    paintStyle.color = e.target.value;
+    context.strokeStyle = e.target.value;
   }
 
   if (e.target.id === "lineWidth") {
-    lineWidth = e.target.value;
+    paintStyle.lineWidth = e.target.value;
   }
 });
 
 const draw = (e) => {
-  if (!isPainting) {
+  if (!paint.active) {
     return;
   }
+
+  paint.move = true;
+  paint.before = {
+    x: paint.pos.x,
+    y: paint.pos.y,
+  };
+  paint.pos.x = e.clientX - canvasOffsetX;
+  paint.pos.y = e.clientY - canvasOffsetY;
+
   ws.send(
     JSON.stringify({
       path: "draw",
-      lineWidth: lineWidth,
-      x: e.clientX - canvasOffsetX,
-      y: e.clientY,
-      color: color,
+      lineWidth: paintStyle.lineWidth,
+      x: paint.pos.x,
+      y: paint.pos.y,
+      color: paintStyle.color,
     })
   );
 
-  ctx.lineWidth = lineWidth;
-  ctx.lineCap = "round";
+  context.lineWidth = paintStyle.lineWidth;
+  context.lineCap = "round";
 
-  ctx.lineTo(e.clientX - canvasOffsetX, e.clientY);
-  ctx.stroke();
+  // context.moveTo(paint.before.x, paint.before.y);
+  context.lineTo(paint.pos.x, paint.pos.y);
+  context.stroke();
   console.log("mousemove");
 };
 
 canvas.addEventListener("mousedown", (e) => {
-  isPainting = true;
-  startX = e.clientX;
-  startY = e.clientY;
+  paint.active = true;
+  paint.before = {
+    x: e.clientX,
+    y: e.clientY,
+  };
+
   console.log("mousedown");
 });
 
 canvas.addEventListener("mouseup", (e) => {
-  isPainting = false;
-  ctx.stroke();
-  ctx.beginPath();
+  paint.active = false;
+  context.stroke();
+  context.beginPath();
   console.log("mouseup");
 });
 
