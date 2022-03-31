6909a73af02fa276b8700c32162c053dc337f0a3
Ermerson Camarão Lima <el.erm.lima@gmail.com>
2022-03-31 13:08:32 -0300

    create: html home page (#13)
    * create: home page
    
    * create: add font file
    
    * create: add links
    
    * delete: font file
    
    * create: home style and script
    
    * refactor: imports
    
    * create: sounds directory, script logic, style and log in div
    
    * create: change function
    
    * fix: WebSocket
    
    * create: keydown event

---
+++ frontend/src/scripts/home.js
@@ -0,0 +1,69 @@
+const nameInput = document.getElementById("name");
+const logInButton = document.getElementById("log-in-button");
+
+//route to verify name
+let wss = new WebSocket(`wss://127.0.0.1:5050`);
+
+const elementsToSound = [nameInput, logInButton];
+
+for (let i in elementsToSound) {
+  elementsToSound[i].addEventListener("mouseenter", () => {
+    const sound = new Audio("./367376__wjoojoo__blip03.wav");
+    sound.play();
+  });
+
+  elementsToSound[i].addEventListener("mouseleave", () => {
+    const sound = new Audio("./49070__moca__mocasg-fxs03.mp3");
+    sound.play();
+  });
+}
+
+const logIn = () => {
+  if (/\s/g.test(nameInput.value)) {
+    document.getElementById("message").innerHTML =
+      "O nome não pode conter espaços em branco";
+    setTimeout(() => {
+      document.getElementById("message").innerHTML = "";
+    }, 1500);
+  } else if (nameInput.value.length <= 0) {
+    document.getElementById("message").innerHTML =
+      "O nome deve conter pelo menos um caractere";
+    setTimeout(() => {
+      document.getElementById("message").innerHTML = "";
+    }, 1500);
+  } else {
+    wss.send(JSON.stringify({ "name": nameInput.value }));
+    wss.onmessage((event) => {
+      if (event.data.message === "OK") {
+        //Ok: player must be redirected to another page if name is unique in the game room
+        document.getElementById("message").innerHTML =
+          "Ok: devemos guardar o nome e mandar o jogador para a página da sala/jogo";
+        setTimeout(() => {
+          document.getElementById("message").innerHTML = "";
+        }, 2000);
+      } else if (event.data.message === "Nome repetido") {
+        //repeated name: player must choose another name
+        document.getElementById("message").innerHTML =
+          "O nome já existe na sala. Escolha outro nome";
+        setTimeout(() => {
+          document.getElementById("message").innerHTML = "";
+        }, 2000);
+      } else {
+        //other possibility: feedback to player
+        document.getElementById("message").innerHTML =
+          "Ocorreu um problema na aplicação. Tente novamente mais tarde";
+        setTimeout(() => {
+          document.getElementById("message").innerHTML = "";
+        }, 2000);
+      }
+    });
+  }
+};
+
+logInButton.addEventListener("click", logIn);
+
+document.addEventListener("keydonw", (e) => {
+  if (e.which === 13) {
+    logIn();
+  };
+});
