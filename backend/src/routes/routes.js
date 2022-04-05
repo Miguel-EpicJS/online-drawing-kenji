198f8070bbd12713bdc94d300f9bc711f3a71f28
CodigoDeSenior <migueljnm840@gmail.com>
2022-04-05 14:30:03 -0300

    bug fixes to page change, login and sounds

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,39 +1,57 @@
 function drawBroadcast(data, ws, wss, WebSocket) {
-  // data is the data received from wss connection
+    // data is the data received from wss connection
 
-  wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN && client.id !== ws.id) {
-      client.send(
-        JSON.stringify({
-          drawing: {
-            ...data,
-            lineWidth: data.lineWidth,
-            x: data.x,
-            y: data.y,
-            color: data.color,
-            text: data.text,
-          },
-          path: "/draw",
-        })
-      );
-    }
-  });
+    wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN && client.id !== ws.id) {
+            client.send(
+                JSON.stringify({
+                    drawing: {
+                        ...data,
+                        lineWidth: data.lineWidth,
+                        x: data.x,
+                        y: data.y,
+                        color: data.color,
+                        text: data.text,
+                    },
+                    ok: true,
+                    path: "/draw",
+                })
+            );
+        }
+    });
 }
 
 function chatBroadcast(data, ws, wss, WebSocket) {
-  wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN && client.id !== ws.id) {
-      client.send(
-        JSON.stringify({
-          msg: {
-            ...data,
-            text: data.text,
-          },
-          path: "/chat",
-        })
-      );
-    }
-  });
+    wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN && client.id !== ws.id) {
+            client.send(
+                JSON.stringify({
+                    msg: {
+                        ...data,
+                        text: data.text,
+                    },
+                    ok: true,
+                    path: "/chat",
+                })
+            );
+        }
+    });
 }
 
-module.exports = { draw: drawBroadcast, chat: chatBroadcast }; // path: function
+function userLogin(data, ws, wss, WebSocket) {
+
+    // login validations
+
+    ws.send(JSON.stringify({
+        msg: {
+            text: "Login Ok",
+        },
+        ok: true,
+        path: "/login"
+    }));
+
+    // salvar no banco
+
+}
+
+module.exports = { draw: drawBroadcast, chat: chatBroadcast, login: userLogin }; // path: function
