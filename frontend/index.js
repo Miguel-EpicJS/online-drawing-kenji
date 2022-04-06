f62aabdeed86a802f43f8ca501c3d4e4a953ef48
Lay <layssa.lima166@gmail.com>
2022-04-06 15:25:56 -0300

    fix: change to class (#25)
    * create: class
    
    * create: class
    
    * fix: delete const elementsSound

--- frontend/index.js
+++ frontend/index.js
@@ -1,193 +1,171 @@
 import "./style.css";
+import { Paint } from "./src/scripts/paint";
+import { CanvasControl } from "./src/scripts/canvas";
+import { wss as ws } from "./src/scripts/home";
+//--------------------------------------------------
+//  criar arquivo config para guardar as variaveis
+//--------------------------------------------------
 
-try {
-    const canvas = document.getElementById("drawing-board");
-    const toolbar = document.getElementById("toolbar");
+class WS {
+  constructor() {
+    this.ws = new WebSocket("wss://localhost:5050");
+    this.username = "";
+    this.channel = "";
+    this.idUser = "";
+    this.messages = [];
+  }
 
-    const context = canvas.getContext("2d");
+  setUsername(_username) {
+    this.username = _username;
+  }
 
-    canvas.width = window.innerWidth - canvasOffsetX;
-    canvas.height = window.innerHeight - canvasOffsetY;
-    const text = document.querySelector(".show-answer");
-    const sendBtn = document.querySelector(".answer-button");
-    const messageBox = document.querySelector(".answer-input");
+  setChannel(_channel) {
+    this.channel = _channel;
+  }
 
-    const canvasOffsetX = canvas.offsetLeft;
-    const canvasOffsetY = canvas.offsetTop;
+  setUserId(_id) {
+    this.idUser = _id;
+  }
 
-    canvas.width = window.innerWidth - canvasOffsetX;
-    canvas.height = window.innerHeight - canvasOffsetY - 200;
+  sendMessage(_message) {
 
-    // User Mock
-    const user = "Player 1";
+  }
 
-    const paintStyle = {
-        lineWidth: 3,
-        color: "#000000",
-    };
-
-    const paint = {
-        active: false,
-        move: false,
-        pos: {
-            x: 0,
-            y: 0,
-        },
-        before: null,
-    };
-
-    /* WS */
-    const ws = new WebSocket("wss://localhost:5050");
-
-    ws.onmessage = (ms) => {
-        const submitedData = JSON.parse(ms.data);
-
-        if (submitedData.path === "/chat") {
-            return show(submitedData.msg.text);
-        }
-
-        if (submitedData.drawing.action === "clear") {
-            return context.clearRect(0, 0, canvas.width, canvas.height);
-        }
-
-        context.strokeStyle = submitedData.drawing.color;
-        context.lineWidth = submitedData.drawing.lineWidth;
-        context.lineCap = "round";
-
-        paint.active = false;
-        context.lineTo(submitedData.drawing.x, submitedData.drawing.y);
-        context.stroke();
-    };
-
-    /* DRAW */
-    toolbar.addEventListener("click", (e) => {
-        if (e.target.id === "clear") {
-            console.log("clear");
-            context.clearRect(0, 0, canvas.width, canvas.height);
-            ws.send(
-                JSON.stringify({
-                    path: "draw",
-                    action: "clear",
-                    lineWidth: paintStyle.lineWidth,
-                    x: 0,
-                    y: 0,
-                    color: paintStyle.color,
-                    user,
-                })
-            );
-        }
-    });
-
-    toolbar.addEventListener("change", (e) => {
-        if (e.target.id === "stroke") {
-            paintStyle.color = e.target.value;
-            context.strokeStyle = e.target.value;
-        }
-
-        if (e.target.id === "lineWidth") {
-            paintStyle.lineWidth = e.target.value;
-        }
-    });
-
-    const draw = (e) => {
-        if (!paint.active) {
-            return;
-        }
-        //Na tela colocar um espaço onde colocarmos isso
-        console.log(`${user} está desenhando`);
-
-        paint.move = true;
-        paint.before = {
-            x: paint.pos.x,
-            y: paint.pos.y,
-        };
-        paint.pos.x = e.clientX - canvasOffsetX;
-        paint.pos.y = e.clientY;
-
-        ws.send(
-            JSON.stringify({
-                path: "draw",
-                action: "drawing",
-                lineWidth: paintStyle.lineWidth,
-                x: paint.pos.x,
-                y: paint.pos.y,
-                color: paintStyle.color,
-                user,
-            })
-        );
-
-        context.lineWidth = paintStyle.lineWidth;
-        context.lineCap = "round";
-
-        // context.moveTo(paint.before.x, paint.before.y);
-        context.lineTo(paint.pos.x, paint.pos.y);
-        context.stroke();
-        console.log("mousemove");
-    };
-
-    canvas.addEventListener("mousedown", (e) => {
-        paint.active = true;
-        paint.before = {
-            x: e.clientX,
-            y: e.clientY,
-        };
-
-        console.log("mousedown");
-    });
-
-    canvas.addEventListener("mouseup", (e) => {
-        paint.active = false;
-        context.stroke();
-        context.beginPath();
-        console.log("mouseup");
-    });
-
-    canvas.addEventListener("mousemove", draw);
-
-    function showMessage(message) {
-        text.innerHTML += `<p id="my">Eu: ${message}<p/>`;
-        text.scrollTop = text.scrollHeight;
-        messageBox.value = "";
-
-        console.log(message);
-    }
-
-    function show(message) {
-        text.innerHTML += `<p id="you">Jogador: ${message}<p/>`;
-        text.scrollTop = text.scrollHeight;
-        messageBox.value = "";
-
-        console.log(message);
-    }
-
-    sendBtn.onclick = function () {
-        if (!ws) {
-            showMessage("No websocket connection :(");
-            return;
-        }
-        let userObj = {
-            nick: "",
-            message: messageBox.value,
-        };
-
-        let user = `${userObj.message}`;
-
-        ws.send(JSON.stringify({ path: "chat", text: user, action: "chat" }));
-        showMessage(messageBox.value);
-    };
-
-
-    /* LEAVE */
-
-    const leaveBtn = document.getElementById("leave");
-
-    leaveBtn.onclick = () => {
-
-        window.location.href = "home.html";
-
-    };
-} catch (error) {
-    console.log(error);
+  getMessage() {}
 }
 
- 
+const canvas = document.getElementById("drawing-board");
+const toolbar = document.getElementById("toolbar");
+const context = canvas.getContext("2d");
+const text = document.querySelector(".show-answer");
+const sendBtn = document.querySelector(".answer-button");
+const messageBox = document.querySelector(".answer-input");
+
+// User Mock
+const user = "Player 1";
+
+const paint = new Paint();
+const board = new CanvasControl(canvas);
+
+/* WS */
+// const ws = new WebSocket("wss://localhost:5050");
+
+ws.onmessage = (ms) => {
+  const submitedData = JSON.parse(ms.data);
+
+  if (submitedData.path === "/chat") {
+    return show(submitedData.msg.text);
+  }
+
+  if (submitedData.drawing.action === "clear") {
+    return board.clearContext();
+  }
+
+  board.strokeStyle(submitedData.drawing.color);
+  board.setLineWidth(submitedData.drawing.lineWidth);
+  board.setLineCap("round");
+
+  paint.activeCursor();
+  board.setLineTO(submitedData.drawing.x, submitedData.drawing.y);
+};
+
+/* DRAW */
+toolbar.addEventListener("click", (e) => {
+  if (e.target.id === "clear") {
+    console.log("clear");
+    board.clearContext();
+
+    ws.send(
+      JSON.stringify({
+        path: "draw",
+        action: "clear",
+        lineWidth: paint.getLineWidth(),
+        x: 0,
+        y: 0,
+        color: paint.getColor(),
+        user,
+      })
+    );
+  }
+});
+
+toolbar.addEventListener("change", (e) => {
+  if (e.target.id === "stroke") {
+    paint.setColor(e.target.value);
+    board.setStrokeStyle(target.value);
+  }
+
+  if (e.target.id === "lineWidth") {
+    paint.setWidth(e.target.value);
+  }
+});
+
+const draw = (e) => {
+  if (!paint.getState()) {
+    return;
+  }
+
+  console.log(`${user} está desenhando`);
+
+  paint.setBeforeCursor();
+  paint.setMoveCursor(e.clientX - board.getCanvasOffsetX(), e.clientY);
+
+  ws.send(
+    JSON.stringify({
+      path: "draw",
+      action: "drawing",
+      lineWidth: paint.getLineWidth(),
+      x: paint.getPos().x,
+      y: paint.getPos().y,
+      color: paint.getColor(),
+      user,
+    })
+  );
+
+  board.setLineWidth(paint.lineWidth);
+  board.setLineCap("round");
+
+  context.lineTo(paint.pos.x, paint.pos.y);
+  context.stroke();
+};
+
+canvas.addEventListener("mousedown", (e) => {
+  paint.activeCursor();
+  paint.setBeforeCursor();
+});
+
+canvas.addEventListener("mouseup", (e) => {
+  paint.disabledCursor();
+  context.stroke();
+  context.beginPath();
+});
+
+canvas.addEventListener("mousemove", draw);
+
+function showMessage(message) {
+  text.innerHTML += `<p id="my">Eu: ${message}<p/>`;
+  text.scrollTop = text.scrollHeight;
+  messageBox.value = "";
+}
+
+function show(message) {
+  text.innerHTML += `<p id="you">Jogador: ${message}<p/>`;
+  text.scrollTop = text.scrollHeight;
+  messageBox.value = "";
+}
+
+sendBtn.onclick = function () {
+  if (!ws) {
+    showMessage("No websocket connection :("); // tentar conectar novamente.
+    return;
+  }
+  let userObj = {
+    nick: "",
+    message: messageBox.value,
+  };
+
+  let user = `${userObj.message}`;
+
+  ws.send(JSON.stringify({ path: "chat", text: user, action: "chat" }));
+  showMessage(messageBox.value);
+};
