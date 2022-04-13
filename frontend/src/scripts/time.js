416623aed69ec42d276a02f2ee4c6de34506b462
Layssa <layssa.lima166@gmail.com>
2022-04-13 01:33:09 -0300

    feat: create time class

---
+++ frontend/src/scripts/time.js
@@ -0,0 +1,59 @@
+class Time {
+  constructor() {
+    this.currentTime = 0;
+    this.time = 60000;
+    this.timerInterval = 1000;
+    this.intervalTimeout
+    this.callbackTimeout = function () {};
+    this.callbackTimeInterval = function () {};
+  }
+
+  setTime() {
+    this.time = 60000;
+  }
+
+  setCallbackTimeout(_callbackTimeout) {
+    this.callbackTimeout = _callbackTimeout;
+  }
+
+  setCallbackTimeInterval(_callbackTimeInterval) {
+    this.callbackTimeInterval = _callbackTimeInterval;
+  }
+
+  startTimer() {
+    this.internalTimer = setInterval(() => {
+      if (this.time <= 0) {
+        clearInterval(this.internalTimer);
+        clearTimeout(this.intervalTimeout);
+        return true;
+      } else {
+        this.callbackTimeInterval();
+        this.currentTime += this.timerInterval;
+        this.time -= this.timerInterval;
+      }
+    }, this.timerInterval);
+
+    this.time = this.time - this.currentTime;
+    this.intervalTimeout = setTimeout(this.callbackTimeout, this.time);
+    return this.time;
+  }
+
+  stopTimer() {
+    clearInterval(this.internalTimer);
+    clearTimeout(this.intervalTimeout);
+    return "Paused";
+  }
+
+  resetTimer() {
+    clearTimeout(this.intervalTimeout);
+    clearInterval(this.internalTimer);
+    this.time = 0;
+    this.timerInterval = 1000;
+    this.currentTime = 0;
+    return "--";
+  }
+
+  getCurrentTime() {
+    return this.time;
+  }
+}
