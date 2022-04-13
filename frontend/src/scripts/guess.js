535b3c721d089db2098267f470902993f41ca4ec
Ermerson Camarão Lima <el.erm.lima@gmail.com>
2022-04-12 22:46:33 -0300

    improve: function

--- frontend/src/scripts/guess.js
+++ frontend/src/scripts/guess.js
@@ -3,17 +3,21 @@ import WORDS from "../../../backend/src/database/words";
 export const word = WORDS[Math.floor(Math.random() * WORDS.length)];
 
 export const verifyGuess = (guess, payload) => {
-  if (guess.split(' ').length > 1) {
-    return { message: 'Dê palpites de apenas uma palvra!', action: 'write'}
-  } 
+  if (guess.split(" ").length > 1) {
+    return {
+      message: "Dê palpites de apenas uma palvra!",
+      action: "write",
+      isCorrect: false,
+    };
+  }
   const compare = (guess) => {
     const newGuess = String(guess);
     return word.variants.find((variant) => variant === newGuess.toLowerCase());
   };
 
   if (!compare(guess) || compare(guess) === undefined) {
-    return { message: "Jogador venceu!", action: "write" };
+    return { message: "Jogador venceu!", action: "write", isCorrect: true };
   } else {
-    return { message: undefined, action: "none" };
+    return { message: undefined, action: "none", isCorrect: false };
   }
 };
