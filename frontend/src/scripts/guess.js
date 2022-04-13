aa3971d0a2b188e6672a9566f470b9b4c90e0dc0
Ermerson Camarão Lima <el.erm.lima@gmail.com>
2022-04-12 23:40:13 -0300

    create: words db and function for draw (#36)
    * create: words for draw
    
    * improve: function
    
    * improve: function
    
    * fix: text

---
+++ frontend/src/scripts/guess.js
@@ -0,0 +1,23 @@
+import WORDS from "../../../backend/src/database/words";
+
+export const word = WORDS[Math.floor(Math.random() * WORDS.length)];
+
+export const verifyGuess = (guess, payload) => {
+  if (guess.split(" ").length > 1) {
+    return {
+      message: "Dê palpites de apenas uma palavra!",
+      action: "write",
+      isCorrect: false,
+    };
+  }
+  const compare = (guess) => {
+    const newGuess = String(guess);
+    return word.variants.find((variant) => variant === newGuess.toLowerCase());
+  };
+
+  if (!compare(guess) || compare(guess) === undefined) {
+    return { message: "Jogador venceu!", action: "write", isCorrect: true };
+  } else {
+    return { message: undefined, action: "none", isCorrect: false };
+  }
+};
